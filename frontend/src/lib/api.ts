d6bbb5f3e0af1e16f60cce8f4168541bf666d92d
DhanaNugraha <dhananugraha1511@gmail.com>
2025-10-07 14:03:31 +0700

    vercel setup

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api/v1/recipes';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1/recipes';
 
 export interface Recipe {
   id: number;
